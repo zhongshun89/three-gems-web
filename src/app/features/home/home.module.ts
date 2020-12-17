77cd3d959957673e6e8da9e43365398a19c812ac
Shun <you@example.com>
2020-12-17 17:09:16 +0800

    add header

--- src/app/features/home/home.module.ts
+++ src/app/features/home/home.module.ts
@@ -1,23 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from '../../shared/shared.module';
+import { HomeRoutingModule } from './home-routing.module';
 
-
-import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 
 
 
 @NgModule({
   declarations: [
-    HeaderComponent,
     HomeComponent,
+    HeaderComponent,
     FooterComponent
   ],
   imports: [
     CommonModule,
-    SharedModule
+    SharedModule,
+    HomeRoutingModule
   ]
 })
 export class HomeModule { }
